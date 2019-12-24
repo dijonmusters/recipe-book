869d21c06c756e8dbcf48c3712cce649e5c1eb0c
Jon Meyers <jon.meyers@rea-group.com>
2019-12-24 14:58:10 +1100

    looks: add switcher for recipe display

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,8 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { RecipeContext } from '../context/RecipeProvider';
 
 const Container = styled.div`
+  position relative;
   display: flex;
   margin: 0.125rem;
   overflow-x: hidden;
@@ -15,19 +16,25 @@ const Content = styled.div`
   -webkit-overflow-scrolling: touch;
 `;
 
-const Panel = styled.div`
-  min-width: 100%;
-  padding: 0 3rem;
+const Page = styled.div`
+  display: flex;
+  justify-content: center;
+  min-width: 90vw;
   scroll-snap-align: center;
-  background-color: white;
-  padding-bottom: 1rem;
 `;
+
+const Panel = styled.div`
+  background-color: white;
+  margin: 2rem 1rem;
+  padding: 2rem 6rem;
+  width: 100%;
+  ${props => props.first && 'margin-left: 2rem'};
+  ${props => props.last && 'margin-right: 2rem'};
+`
 
 const Title = styled.h1`
   font-family: 'Dancing Script', cursive;
   text-align: center;
-  padding: 1rem 0;
-  margin-bottom: 0;
   font-weight: 100;
   color: #999;
   font-size: 3rem;
@@ -54,16 +61,50 @@ const Item = styled.span`
   display: block;
   margin: 0.5rem 0;
   flex: 1;
+  color: #555;
 `;
+
+const Switcher = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 0.5rem;
+`
+
+const Dot = styled.div`
+  background-color: ${props => props.selected ? 'rgba(0, 0, 0, 0.4)' : 'rgba(0, 0, 0, 0.2)'};
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  margin: 0 0.125rem;
+`
 
 const Recipe = ({ method, ingredients, name }) => {
   const [selectedIngredients, setSelectedIngredients] = useState([]);
   const [selectedSteps, setSelectedSteps] = useState([]);
   const { setSelectedRecipe } = useContext(RecipeContext);
+  const [selected, setSelected] = useState(true);
+  const panelRef = useRef();
 
   useEffect(() => {
     setSelectedRecipe(name);
   }, []);
+
+  useEffect(() => {
+    const config = {
+      threshold: [0.25, 0.75]
+    };
+
+    const observer = new IntersectionObserver((entry, observer) => {
+      setSelected(entry[0].isIntersecting)
+    }, config);
+    if (panelRef && panelRef.current) {
+      observer.observe(panelRef.current)
+    }
+  }, [])
 
   const handleSelectIngredient = ingredient => {
     setSelectedIngredients(
@@ -107,19 +148,24 @@ const Recipe = ({ method, ingredients, name }) => {
   return (
     <Container>
       <Content>
-        <Panel>
-          <Title>
-            ingredients
-          </Title>
-          {ingredients.map(renderIngredient)}
-        </Panel>
-        <Panel>
-          <Title>
-            method
-          </Title>
-          {method.map(renderStep)}
-        </Panel>
+        <Page>
+          <Panel ref={panelRef} first>
+            <Title>
+              ingredients
+            </Title>
+            {ingredients.map(renderIngredient)}
+          </Panel>
+        </Page>
+        <Page>
+          <Panel last>
+            <Title>
+              method
+            </Title>
+            {method.map(renderStep)}
+          </Panel>
+        </Page>
       </Content>
+      <Switcher><Dot selected={selected} /><Dot selected={!selected} /></Switcher>
     </Container>
   );
 }
